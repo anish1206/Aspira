380aebe604be367eadb59a08a35a22a9f2b38099
anish1206 <anishkshirsagar120306@gmail.com>
2025-09-01 23:36:43 +0530

    security3

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,15 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Replace with your Firebase config credentials
+// Firebase config from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyA3EmCxGkk6trnlv9M5O3kMembDdwFZ8qQ",
-  authDomain: "mindsync-11947.firebaseapp.com",
-  projectId: "mindsync-11947",
-  storageBucket: "mindsync-11947.firebasestorage.app",
-  messagingSenderId: "815392141815",
-  appId: "1:815392141815:web:212e9bb66148815dca3ef2",
-  measurementId: "G-SFPYKKV2SN"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
